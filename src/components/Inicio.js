c6180a88da9aa40e727422d6dd9d11752af22314
Sliferyfire <sliferyfire@gmail.com>
2024-01-24 13:32:45 -0600

    #

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -1,5 +1,42 @@
+import { useEffect } from "react";
+import { useState } from "react"
+
+var data=[
+    {
+        id: 100,
+        noticia:"Mejores alumnos de la UTSJR",
+        autor: "Abc news"
+    },
+    {
+        id: 200,
+        noticia:"UTSJR abre nueva carrera",
+        autor: "MNM news"
+    },
+    {
+        id: 300,
+        noticia:"Becas para alumnos de la UTSJR",
+        autor: "Zxy news"
+    },
+]
+
 export function Inicio(){
+    const[dataNews, setDataNews] = useState(data);
+    useEffect(()=>{
+        setDataNews(data);
+    }, []);
+
+    const lista = dataNews.map((new1, id)=>{
+        return(
+            <div key={id}>
+                <div>{new1.id}</div>
+                <div>{new1.noticia}</div>
+                <div>{new1.autor}</div>
+            </div>
+        );
+    });
     return(
-        <h1>Inicio</h1>
+        <div>
+            {lista}
+        </div>
     );
 }
